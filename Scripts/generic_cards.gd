83f4a2239cf762d9fcdf71cbfdf7955d8cbd6b50
Awwi2 <l.behrendt@mtsl.de>
2025-11-24 18:45:54 +0100

    Added ability to create Cards, change folder structure

---
+++ Scripts/generic_cards.gd
@@ -0,0 +1,18 @@
+extends Resource
+class_name Card
+
+@export var name: String
+@export var desc: String
+@export var eff_1: String
+@export var eff_2: String
+@export var eff_3: String
+
+@export var img: CompressedTexture2D
+
+@export var money_mod: int
+@export var pop_mod: int
+@export var rent_mod: int
+@export var risk_mod: int
+@export var mpp_mod: int
+
+@export var rarity: int #unused in original version
