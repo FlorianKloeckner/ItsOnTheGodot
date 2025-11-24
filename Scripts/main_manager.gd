466702987a60bb2ae0dc1467a09a0ad04a47348b
Dudelord <85459353+FlorianKloeckner@users.noreply.github.com>
2025-11-24 21:13:13 +0100

    addcustomers, some methods

--- Scripts/main_manager.gd
+++ Scripts/main_manager.gd
@@ -1,7 +1,7 @@
 extends Node2D
-var money: int = 0
-var popularity: int = 0
-var risk: int = 0
+var money: int = 1000
+var popularity: int = 70
+var risk: int = 1000
 var money_per_customer: int = 10
 
 #-------Sounds-----------
@@ -9,7 +9,8 @@ const SFX_COIN_DOUBLE_3 = preload("res://Assets/Sounds/sfx_coin_double3.wav")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	set_popularity(10)
+	set_money_per_customer(10)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,9 +18,31 @@ func _process(delta: float) -> void:
 	pass
 
 
+func set_money_per_customer(amount: int):
+	money_per_customer = amount
+	$"Background/Info Sprites/MoneyPerCustomerSprite/Label".text = str(money_per_customer)
+	
+	
+func set_popularity(amount: int):
+	popularity = amount
+	$NPCSpawner.set_popularity(amount)
+	$"Background/Info Sprites/PopularitySprite/Label".text = str(popularity)
+
+#adds money, if amount is negative, it subtracts
+func add_money(amount: int):
+	var tot_money = get_money()
+	set_money(tot_money + amount)
+	if tot_money + amount < 0:
+		return false #for alerting when balance negative
+
+func set_money(amount: int):
+	money = amount
+	$"Background/Info Sprites/MoneySprite/Label".text = str(amount)
+
+func get_money():
+	return money
+	
 func _on_npc_spawner_customer_entered() -> void:
-	money += money_per_customer
-	print(money)
+	add_money(money_per_customer)
 	$AudioStreamPlayer2D.stream = SFX_COIN_DOUBLE_3
 	$AudioStreamPlayer2D.play()
-	
--- Scripts/npc_spawner.gd
+++ Scripts/npc_spawner.gd
@@ -1,6 +1,10 @@
 extends Node2D
 signal customer_entered
 var HAT_MAN = preload("res://Scenes/HatMan.tscn")
+var  APRON_GUY = preload("res://Scenes/ApronGuy.tscn")
+var WOMAN = preload("res://Scenes/Woman.tscn")
+
+var popularity: int
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,11 +12,24 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var rand = RandomNumberGenerator.new()
-	if rand.randf() < 0.05 :
-		var instance = HAT_MAN.instantiate()
-		add_child(instance)
+	spawn_npc()
 	
+
+func spawn_npc():
+	var rand = RandomNumberGenerator.new()
+	var instance
+	if rand.randf() < float(popularity)/1000.0:
+		var npc_num = rand.randi_range(1, 3)
+		if npc_num == 1:
+			instance = HAT_MAN.instantiate()
+		elif npc_num == 2:
+			instance = APRON_GUY.instantiate()
+		else:
+			instance = WOMAN.instantiate()
+		add_child(instance)
+
+func set_popularity(amount: int):
+	popularity = amount
 	
 
 
